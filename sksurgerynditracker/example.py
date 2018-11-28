08f86c680fba704f2de52f50a69893fedaed2040
Stephen Thompson <s.thompson@ucl.ac.uk>
2018-11-28 12:46:42 +0000

    Issue #1 changed nditracker to configure with dictionary and hopefully support multiple rom files

--- sksurgerynditracker/example.py
+++ sksurgerynditracker/example.py
@@ -3,17 +3,18 @@
 #the configuration. 
 #        "ip" : "169.254.60.146", this is what comes up via zero conf.
 settings={
-        "ip" : "192.168.2.17",
+        "tracker type": "vega",
+        "ip address" : "192.168.2.17",
         "port" : 8765, 
-        "romfiles" : "/home/thompson/src/ndicapi/Applications/8700339_smallblue-150130.rom"
+        "romfiles" : [ "/home/thompson/src/ndicapi/Applications/8700339_smallblue-150130.rom" ]
         }
 
 import nditracker
 import time
 
 tracker=nditracker.ndiTracker()
-tracker.Connect(settings.get("ip"), settings.get("port"))
-tracker.ReadSROMsFromFile([settings.get("romfiles")])
+tracker.Connect(settings)
+tracker.ReadSROMsFromFile()
 tracker._InitialisePorts()
 tracker._EnableTools()
 tracker.StartTracking()
--- sksurgerynditracker/nditracker.py
+++ sksurgerynditracker/nditracker.py
@@ -51,8 +51,11 @@ class ndiTracker:
         self.toolDescriptors = []
         self.trackerType = None
 
-    def Connect (self, ip , port ):
-        self.device = ndiOpenNetwork(ip, port)
+    def Connect (self, configuration ):
+        self._Configure( configuration )
+        if self.trackerType == "vega":
+            self.device = ndiOpenNetwork(self.ip_address, self.port)
+
         if not self.device:
             raise IOError(
             'Could not connect to NDI device found on '
@@ -65,10 +68,45 @@ class ndiTracker:
             'Error when sending command: '
             '{}'.format(ndiErrorString(error)))
 
+    def _Configure(self, configuration):
+        """ Reads a configuration dictionary
+        describing the tracker configuration.
+        and sets class variables.
+        """
+        #This is a bit clunky, can we do get and test in
+        # one line and Is there a way to make it case insensitive?
+        if not "tracker type" in configuration:
+            raise KeyError ( "Configuration must contain 'Tracker type'" )
+
+        trackerType = configuration.get("tracker type")
+        if trackerType == "vega" or trackerType == "polaris" or \
+            trackerType == "aurora" or trackerType == "dummy":
+            self.trackerType = trackerType
+        else:
+            raise ValueError ( "Supported trackers are 'vega', 'aurora', 'polaris', and 'dummy'" )
+
+        if self.trackerType == "vega":
+            if not "ip address" in configuration:
+                raise KeyError ( "Configuration for vega must contain 'ip address'" )
+            self.ip_address = configuration.get("ip address")
+            if not "port" in configuration:
+                self.port = 8765
+            else:
+                self.port = configuration.get("port")
+
+        if self.trackerType == "vega" or self.trackerType == "polaris":
+            if "romfiles" not in configuration:
+                raise KeyError ( "Configuration for vega and polaris must contain a list of 'romfiles'" )
+            for romfile in configuration.get("romfiles"):
+                self.toolDescriptors.append( { "description" : romfile } )
+
+        if self.trackerType == "aurora" or  self.trackerType == "polaris" or self.trackerType == "dummy":
+            raise NotImplementedError ( " Polaris, aurora, and dummy not implemented yet")
+
     def Close (self):
         ndiCloseNetwork(self.device)
 
-    def ReadSROMsFromFile (self,  sromFilenames ):
+    def ReadSROMsFromFile (self):
         #basically want to implement NDICapitracker::EnableToolPorts
         # // stop tracking
         #if (this->IsDeviceTracking) //this is an integer flag maintaned my m_Tracker. We can do that I guess.
@@ -85,21 +123,20 @@ class ndiTracker:
             ndiCommand(self.device,"PHF:%02X",portHandle)
             self._CheckForErrors('freeing port handle {}.'.format(toolIndex))
 
-        for sromfilename in sromFilenames:
+        for tool in self.toolDescriptors:
             #no wired tools
             #get a port handle, there's a lot of wildcards in the
             #next PHRQ command?
             ndiCommand(self.device, 'PHRQ:*********1****')
             #not sure this is the python module
             portHandle = ndiGetPHRQHandle(self.device);
-            self.toolDescriptors.append( { "portHandle" : portHandle,
-                    "description" : sromfilename } )
+            tool.update ({ "portHandle" : portHandle } )
 
-            print_("Loading  " , sromfilename, " to port handle ", portHandle);
+            print_("Loading  " , tool.get("description"), " to port handle ", portHandle);
             self._CheckForErrors('getting srom file port handle {}.'.format(portHandle))
 
             #this returns some sortof ndibit field? What do we do with it
-            reply=ndiPVWRFromFile(self.device, portHandle, sromfilename)
+            reply=ndiPVWRFromFile(self.device, portHandle, tool.get("description"))
             print_(reply)
             self._CheckForErrors('setting srom file port handle {}.'.format(portHandle))
 
@@ -112,7 +149,7 @@ class ndiTracker:
         numberOfTools=ndiGetPHSRNumberOfHandles(self.device)
         print_("Initialising " , numberOfTools, " on device")
         for tool in self.toolDescriptors:
-            ndiCommand(self.device,"PINIT:%02X",tool.portHandle)
+            ndiCommand(self.device,"PINIT:%02X",tool.get("portHandle"))
             self._CheckForErrors('Initialising port handle {}.'.format(tool.get("portHandle")))
 
     def _EnableTools (self):
@@ -124,7 +161,7 @@ class ndiTracker:
             #going to implement default mode
             #ndiCommand(self.device,"PHINF:%02X0001",portHandle)
             mode='D'
-            ndiCommand(self.device,"PENA:%02X%c",tool.portHandle,mode);
+            ndiCommand(self.device,"PENA:%02X%c",tool.get("portHandle"),mode);
             self._CheckForErrors('Enabling port handle {}.'.format(tool.get("portHandle")))
 
         ndiCommand(self.device,"PHSR:04")
