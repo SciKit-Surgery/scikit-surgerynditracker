927e0bde55565872f5c95fa270e96b63e8eae5c6
Stephen Thompson <s.thompson@ucl.ac.uk>
2019-01-10 16:19:42 +0000

    #1 style changes for example

--- sksurgerynditracker/example.py
+++ sksurgerynditracker/example.py
@@ -1,26 +1,30 @@
 #! /user/bin/python
 
-#the configuration. 
-#        "ip" : "169.254.60.146", this is what comes up via zero conf.
-settings_vega={
+"""Brief example showing how to initialise, configure, and communicate
+with NDI Polaris, Vega, and Aurora trackers."""
+
+#configuration.
+SETTINGS_VEGA = {
         "tracker type": "vega",
         "ip address" : "192.168.2.17",
-        "port" : 8765, 
-        "romfiles" : [ "/home/thompson/src/ndicapi/Applications/8700339_smallblue-150130.rom" ,
-            "../8700339.rom" ]
+        "port" : 8765,
+        "romfiles" : [
+            "../data/8700339_smallblue-150130.rom",
+            "../data/8700339.rom"]
         }
 
-settings_polaris={
+SETTINGS_POLARIS = {
         "tracker type": "polaris",
-        "romfiles" : [ "/home/thompson/src/ndicapi/Applications/8700339_smallblue-150130.rom" ,
-            "../8700339.rom" ]
+        "romfiles" : [
+            "../data/8700339_smallblue-150130.rom",
+            "../data/8700339.rom"]
         }
 
-settings_aurora={
+SETTINGS_AURORA = {
         "tracker type": "aurora",
         }
 
-settings_dummy={
+SETTINGS_DUMMY = {
         "tracker type": "dummy",
         }
 
@@ -28,17 +32,15 @@ import nditracker
 import time
 import six
 
-tracker=nditracker.ndiTracker()
-tracker.Connect(settings_vega)
+tracker = nditracker.ndiTracker()
+tracker.Connect(SETTINGS_VEGA)
 
 tracker.StartTracking()
 
-six.print_ (tracker.GetToolDescriptionsAndPortHandles())
-for _ in range (20):
-    six.print_ (tracker.GetFrame())
+six.print_(tracker.GetToolDescriptionsAndPortHandles())
+for _ in range(20):
+    six.print_(tracker.GetFrame())
     time.sleep(0.300333)
 
 tracker.StopTracking()
 tracker.Close()
-
-
