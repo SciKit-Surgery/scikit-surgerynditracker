43748a95b9fb494dff4c7f02eb5397cd2367d22f
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-06-23 15:37:37 +0100

    Issue #47 added tests to check calls to ndiCommand

--- tests/test_sksurgerynditracker_withmockndicapy.py
+++ tests/test_sksurgerynditracker_withmockndicapy.py
@@ -2,6 +2,7 @@
 
 """scikit-surgerynditracker tests using a mocked ndicapy"""
 import ndicapy
+from mock import call
 from sksurgerynditracker.nditracker import NDITracker
 
 #configuration.
@@ -95,5 +96,26 @@ def test_connect_serial_mock(mocker):
     mocker.patch('ndicapy.ndiPVWRFromFile')
     mocker.patch('ndicapy.ndiGetPHSRHandle', mockndiGetPHSRHandle)
     mocker.patch('ndicapy.ndiVER', mockndiVER)
+    spy = mocker.spy(ndicapy, 'ndiCommand')
     tracker = NDITracker(SETTINGS_POLARIS)
+
+    assert spy.call_count == 18
+    assert spy.call_args_list[0] == call(True, 'INIT:')
+    assert spy.call_args_list[1] == call(True, 'COMM:50000')
+    assert spy.call_args_list[2] == call(True, 'PHSR:01')
+    assert spy.call_args_list[3] == call(True, 'PHF:00')
+    assert spy.call_args_list[4] == call(True, 'PHF:01')
+    assert spy.call_args_list[5] == call(True, 'PHF:02')
+    assert spy.call_args_list[6] == call(True, 'PHF:03')
+    assert spy.call_args_list[7] == call(True, 'PHRQ:*********1****')
+    assert spy.call_args_list[8] == call(True, 'PHRQ:*********1****')
+    assert spy.call_args_list[9] == call(True, 'PHSR:01')
+    assert spy.call_args_list[10] == call(True, 'PHSR:02')
+    assert spy.call_args_list[11] == call(True, 'PINIT:00')
+    assert spy.call_args_list[12] == call(True, 'PINIT:00')
+    assert spy.call_args_list[13] == call(True, 'PHSR:03')
+    assert spy.call_args_list[14] == call(True, 'PENA:00D')
+    assert spy.call_args_list[15] == call(True, 'PENA:01D')
+    assert spy.call_args_list[16] == call(True, 'PENA:02D')
+    assert spy.call_args_list[17] == call(True, 'PENA:03D')
     del tracker
