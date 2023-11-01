77baf8e1654a9d7e831d45043fe344f7313fc9d1
Stephen Thompson <s.thompson@ucl.ac.uk>
2023-11-01 16:40:30 +0000

    Revert "Test with new ndicapi version"
    This reverts commit 07f124c4be24b851524630267cbc70135bdb7d0f.
    
    The ndicapi api hasn't changed so there should be no need to specify
    vesion >=3.7.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         'six>=1.10',
         'numpy>=1.11',
-        'ndicapi==3.7.0rc1',
+        'ndicapi>=3.2.6',
         'scikit-surgerycore>=0.7.0',
         'pyserial'
     ],
