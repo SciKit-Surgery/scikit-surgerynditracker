7a694561771af8b2e7df001aae7a72f2de5ec1b9
Stephen Thompson <s.thompson@ucl.ac.uk>
2024-02-21 14:13:56 +0000

    Issue #81 updated versioneer and use sksurgerycore with updated versioneer, 0.7.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'six>=1.10',
         'numpy>=1.11',
         'ndicapi>=3.2.6',
-        'scikit-surgerycore>=0.7.0',
+        'scikit-surgerycore>=0.7.1',
         'pyserial'
     ],
 )
